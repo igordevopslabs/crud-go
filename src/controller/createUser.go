97c4f5ed6cd139aae5745f4fa442ef16d2069d50
igoritos22 <igoritos_22@hotmail.com>
2024-02-28 22:20:12 -0300

    validando campos, criando usuario

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,26 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/igordevopslabs/crud-go/src/configuration/rest_err"
+	"github.com/igordevopslabs/crud-go/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Voce chamou a rota incorretamente")
-	c.JSON(err.Code, err)
+	//variavel do tipo UserRequest para acessar os campos da struct de user_request.go
+	var userRequest request.UserRequest
+
+	//se der qualquer erro nos campos que estou recebendo atraves da req vou retornar um BadRequest
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("there is something wrong with filed, error=%s", err.Error()))
+
+		//aqui eu preciso dizer para o gin retornar em json qual o erro e status code.
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
